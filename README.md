818a5caf8741286484f7f14af9b98e04eb1106b3
GwakaMolie <bazme99@gmail.com>
2022-05-03 10:11:24 +0100

    Strip down

--- README.md
+++ README.md
@@ -1,21 +1,17 @@
 # bar
 
-This is a small Rust program that uses [unixbar] to generate some status info. It is used by
-[awesome] to show audio volume, battery level, and weather info.
+A stripped down version of the bar by [wezm](https://github.com/wezm) to generate
+system info (battery, CPU usage, Volume) for 
+[awesome](https://github.com/awesomeWM/awesome/) using [unixbar](https://github.com/myfreeweb/unixbar).
 
-<img src="https://raw.githubusercontent.com/wezm/bar/master/screenshot.png" alt="Screen show of bar in action" width="351" />
+<img src=" https://raw.githubusercontent.com/GwakaMolie/bar/master/screenshot.png " alt="Screen show of bar in action" width="351" />
 
-See my [rc.lua] for how it's used from awesome.
+# Installation
+```
+cd ~/.config/awesome
+git clone https://github.com/GwakaMolie/bar.git && cd bar
+cargo build --release
+```
+## Awesome Configuration
+See my [rc.lua](https://github.com/GwakaMolie/dotfiles/blob/d03d2c465be2498bb8c3bb3d1c14ee538e0313c5/awesome/.config/awesome/rc.lua#L104:L115) for how it's used from awesome.
 
-## Dependencies
-
-### FreeBSD
-
-* dbus
-* pkgconfig
-* xcb
-  * python3 :(
-
-[unixbar]: https://github.com/myfreeweb/unixbar
-[awesome]: https://awesomewm.org/
-[rc.lua]: https://github.com/wezm/dotfiles/blob/3e773263c0222a8b956923164e8e08438865cd55/config/awesome/rc.lua#L215-L227
--- src/main.rs
+++ src/main.rs
@@ -1,193 +1,50 @@
-use std::env;
 use systemstat::{Platform, System};
 use unixbar::*;
 
-mod garage;
-mod weather;
-
-use crate::garage::{Door, DoorState};
-use crate::weather::Observation;
-
-#[derive(Default)]
-struct Config {
-    audio: bool,
-    battery: bool,
-    weather: bool,
-    cpu_temp: bool,
-    garage: bool,
-}
-
-impl Config {
-    fn from_args() -> Self {
-        let mut config = Config::default();
-
-        for arg in env::args().skip(1) {
-            match arg.as_str() {
-                "-a" => config.audio = true,
-                "-b" => config.battery = true,
-                "-w" => config.weather = true,
-                "-c" => config.cpu_temp = true,
-                "-g" => config.garage = true,
-                _ => (),
-            }
-        }
-
-        config
-    }
-}
-
 fn main() {
-    let config = Config::from_args();
 
     let mut bar = UnixBar::new(AwesomeFormatter::new());
 
-    if config.audio {
-        bar.add(Volume::new(default_volume(), |volume| {
-            if volume.muted {
-                bfmt![text["🔇"]]
-            } else {
-                let volume = (volume.volume * 100.).round() as i32;
-                if volume < 1 {
-                    bfmt![fmt["🔈{}", volume]]
-                } else if volume < 25 {
-                    bfmt![fmt["🔉{}", volume]]
-                } else {
-                    bfmt![fmt["🔊 {}", volume]]
-                }
-            }
-        }));
-    }
+    bar
+    // Volume
+    .add(Volume::new(default_volume(), |volume| {
+        if volume.muted {
+            bfmt![text["mute"]]
+        } else {
+            let volume = (volume.volume * 100.).round() as i32;
+            bfmt![fmt["V {}%", volume]]
+        }
+    }))
 
-    if config.battery {
-        bar.add(Periodic::new(Duration::from_secs(30), || {
-            let system = System::new();
-            let symbol = system.on_ac_power().map(|on_ac_power| if on_ac_power { "🔌" } else { "🔋" });
-            match (symbol, system.battery_life()) {
-                (Ok(symbol), Ok(battery)) =>
-                    bfmt![ pad[1] fmt["{} {}%", symbol, (battery.remaining_capacity * 100.).round() as i32]],
-                (Ok(symbol), Err(_)) =>
-                    bfmt![ pad[1] text[symbol]],
-                (Err(err), _) =>
-                    bfmt![fg["#bb1155"] pad[1] text[err.to_string()]],
-            }}
-        ));
-    }
+    // Battery
+    .add(Periodic::new(Duration::from_secs(30), || {
+        let system = System::new();
+        let symbol = system.on_ac_power().map(|on_ac_power| if on_ac_power { "" } else { "B" });
+        match (symbol, system.battery_life()) {
+            (Ok(symbol), Ok(battery)) =>
+                bfmt![ pad[1] fg["#ff5555"] fmt["{} {}%", symbol, (battery.remaining_capacity * 100.).round() as i32]],
+            (Ok(symbol), Err(_)) =>
+                bfmt![ pad[1] text[symbol]],
+            (Err(err), _) =>
+                bfmt![fg["#bb1155"] pad[1] text[err.to_string()]],
+        }}
+    ))
 
-    if config.weather {
-        bar.add(Periodic::new(Duration::from_secs(5 * 60), || {
-            let weather = weather::Client::new();
-
-            let observation = weather
-                .observations()
-                .ok()
-                .and_then(|observations| observations.into_iter().nth(0));
-
-            bfmt![ pad[1] fmt["{}", format_observation(&observation)] ]
-        }));
-    }
-
-    if config.cpu_temp {
-        bar.add(Periodic::new(
-            Duration::from_secs(5),
-            || match System::new().cpu_temp() {
-                Ok(temp) => bfmt![pad[1] fmt["{}°C", temp]],
-                Err(_) => bfmt![fg["#bb1155"] pad[1] text["cpu temp error"]],
-            },
-        ));
-    }
-
-    if config.garage {
-        bar.add(Periodic::new(Duration::from_secs(15), || {
-            let garage = garage::Client::new();
-
-            let door = garage.state().ok();
-
-            let emoji = match door {
-                Some(Door {
-                    state: DoorState::Open,
-                    ..
-                }) => "🪟",
-                Some(Door {
-                    state: DoorState::Closed,
-                    ..
-                }) => "🚪",
-                Some(Door {
-                    state: DoorState::Unknown,
-                    ..
-                }) => "🚪❓",
-                None => "🚪⁉️",
-            };
-
-            bfmt![ pad[1] fmt["{}", emoji] ]
-        }));
-    }
-
-    //     .register_fn("prev", move || { MPRISMusic::new().prev(); })
-    //     .register_fn("play_pause", move || { MPRISMusic::new().play_pause(); })
-    //     .register_fn("next", move || { MPRISMusic::new().next(); })
-    //     .add(Music::new(MPRISMusic::new(),
-    //     |song| {
-    //         if let Some(playing) = song.playback.map(|playback| playback.playing) {
-    //             bfmt![
-    //                 fg["#bbbbbb"]
-    //                     multi[
-    //                     (click[MouseButton::Left => fn "prev"] no_sep text["[|<]"]),
-    //                     (click[MouseButton::Left => fn "play_pause"]
-    //                      no_sep text[if playing { "[||]" } else { "[>]" }]),
-    //                     (click[MouseButton::Left => sh format!("firefox 'https://musicbrainz.org/artist/{}'",
-    //                                                            song.musicbrainz_artist.unwrap_or("".to_owned()))]
-    //                      no_sep pad[4] text[song.artist]),
-    //                      (no_sep pad[4] text["-"]),
-    //                      (click[MouseButton::Left => sh format!("firefox 'https://musicbrainz.org/recording/{}'",
-    //                                                             song.musicbrainz_track.unwrap_or("".to_owned()))]
-    //                       no_sep text[song.title]),
-    //                      (click[MouseButton::Left => fn "next"] text["[>|]"])
-    //                     ]
-    //             ]
-    //         } else {
-    //             bfmt![click[MouseButton::Left => sh "rhythmbox"]
-    //                 fg["#bbbbbb"] text["[start music]"]]
-    //         }
-    //     }
+    // MPD
+    // .register_fn("prev", move || { MPDMusic::new().prev(); })
+    // .register_fn("play_pause", move || { MPDMusic::new().play_pause(); })
+    // .register_fn("next", move || { MPDMusic::new().next(); })
+    // .add(Music::new(MPDMusic::new(),
+    //     |song| bfmt![ fmt["{}", song.artist]]
     // ))
 
-    // .add(Text::new(bfmt![click[MouseButton::Left => sh "notify-send hi"]
-    //                click[MouseButton::Right => sh "notify-send 'what?'"]
-    //                fg["#11bb55"] text[" Hello World! "]]))
-
-    //         .add(Bspwm::new(|bsp| Format::Concat(bsp.desktops.iter().map(|d| Box::new({
-    //                 let bg = if d.focused { "#99aa11" } else { "#111111" };
-    //                 bfmt![click[MouseButton::Left => sh format!("bspc desktop -f {}", d.name)]
-    //                       bg[bg] fmt[" {} ", d.name]]
-    //             })).collect())))
-
-    // .add(Text::new(bfmt![right]))
-
-    // .add(Periodic::new(
-    //         Duration::from_secs(15),
-    //         || match System::new().memory() {
-    //             Ok(mem) => bfmt![bg["#556677"] fmt[" {}/{} RAM ", mem.free.to_string(false).replace(" GB", ""), mem.total]],
-    //             Err(_) => bfmt![fg["#bb1155"] text["error"]],
-    //         }))
-
-    // .add(Delayed::new(
-    //     Duration::from_secs(5),
-    //     || System::new().cpu_load_aggregate().unwrap(),
-    //     |res| match res {
-    //         Ok(cpu) => bfmt![fg["#99aaff"] fmt[" {:04.1}% CPU ", (1.0 - cpu.idle) * 100.0]],
-    //         Err(_) => bfmt![fg["#bb1155"] text["error"]],
-    //     }))
+    // CPU Usage
+    .add(Delayed::new(Duration::from_secs(5),
+        || System::new().cpu_load_aggregate().unwrap(),
+        |res| match res {
+            Ok(cpu) => bfmt![fmt[" C {}% ", ((1.0 - cpu.idle) * 100.0) as i32]],
+            Err(_) => bfmt![fg["#bb1155"] text["error"]],
+    }));
 
     bar.run_no_stdin();
-}
-
-pub fn format_observation(observation: &Option<Observation>) -> String {
-    if let Some(o) = observation {
-        format!(
-            "🌡{}°c ({}°c)  🌧 ️{}mm  💧{}°c  🌫 ️{}%  🛩 ️{}km/h",
-            o.air_temp, o.apparent_t, o.rain_trace, o.dewpt, o.rel_hum, o.wind_spd_kmh
-        )
-    } else {
-        format!("🌡 --")
-    }
 }
